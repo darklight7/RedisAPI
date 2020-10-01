e0e4ed45bd4622d1412fa6d835d0d0905ab1caa2
Siddhant Nagelia <siddhant.nagelia@olx.com>
2020-10-02 00:12:12 +0530

    Updated config filw

--- src/main/java/com/redis/RedisAPI/config/SpringConfig.java
+++ src/main/java/com/redis/RedisAPI/config/SpringConfig.java
@@ -1,5 +1,20 @@
 package com.redis.RedisAPI.config;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SpringConfig {
+    @Value("${redis.host}")
+    private String host;
+    @Value("${redis.password}")
+    private String password;
+    @Value("${redis.port}")
+    private int port;
+
+    @Value("${redis.jedis.pool.max-total}")
+    private int maxTotal;
+    @Value("${redis.jedis.pool.max-idle}")
+    private int maxIdle;
+    @Value("${redis.jedis.pool.min-idle}")
+    private int minIdle;
     
 }
