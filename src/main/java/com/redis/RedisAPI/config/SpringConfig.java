1047955850e32b9770d18817345e43abc0e9dfec
Siddhant Nagelia <siddhant.nagelia@olx.com>
2020-10-02 00:32:34 +0530

    Updated the jedis tamplate

--- src/main/java/com/redis/RedisAPI/config/SpringConfig.java
+++ src/main/java/com/redis/RedisAPI/config/SpringConfig.java
@@ -1,6 +1,15 @@
 package com.redis.RedisAPI.config;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.util.StringUtils;
 
 public class SpringConfig {
     @Value("${redis.host}")
@@ -16,5 +25,35 @@ public class SpringConfig {
     private int maxIdle;
     @Value("${redis.jedis.pool.min-idle}")
     private int minIdle;
-    
+    @Bean
+    public JedisClientConfiguration getJedisClientConfiguration() {
+        JedisClientConfiguration.JedisPoolingClientConfigurationBuilder JedisPoolingClientConfigurationBuilder = (JedisClientConfiguration.JedisPoolingClientConfigurationBuilder) JedisClientConfiguration
+                .builder();
+        GenericObjectPoolConfig GenericObjectPoolConfig = new GenericObjectPoolConfig();
+        GenericObjectPoolConfig.setMaxTotal(maxTotal);
+        GenericObjectPoolConfig.setMaxIdle(maxIdle);
+        GenericObjectPoolConfig.setMinIdle(minIdle);
+        return JedisPoolingClientConfigurationBuilder.poolConfig(GenericObjectPoolConfig).build();
+    }
+
+    @Bean
+    public JedisConnectionFactory getJedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        if (!StringUtils.isEmpty(password)) {
+            redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+        }
+        redisStandaloneConfiguration.setPort(port);
+        return new JedisConnectionFactory(redisStandaloneConfiguration, getJedisClientConfiguration());
+    }
+
+    @Bean
+    public RedisTemplate redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(getJedisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+
 }
